a09b361d23ce30c16efae4bf7850d44d8360d7a5
unknown <stockjanitor@gmail.com>
2022-08-11 00:44:58 -0700

    1_twoSum

---
+++ 1_TwoSums.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # initiate dictionary
+        values = dict()
+
+        # return index and value of list
+        for index, value in enumerate(nums):
+            
+            # diff between target and value
+            diff = target - value
+
+            # check diff in values, if true, return index of the diff and current index
+            if diff in values:
+                return [values[diff], index]
+            
+            # if not in dic, append current value and assign current index
+            values[value] = index
+        return []
