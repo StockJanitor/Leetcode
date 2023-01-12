ea7f17fffab26e739fdf2bd592cf367807ff36bd
unknown <stockjanitor@gmail.com>
2023-01-12 11:30:20 -0800

    morefuncs

--- data_structure/array.cpp
+++ data_structure/array.cpp
@@ -9,6 +9,7 @@ struct Array
     int length; // current length
 };
 
+////////////////////////////////////////////// Display //////////////////////////////////////////////
 void Display(struct Array arr)
 {
     int i;
@@ -68,11 +69,83 @@ int Delete(struct Array *arr, int index)
     return 0;
 }
 
+
+////////////////////////////////////////////// Get //////////////////////////////////////////////
+int Get(struct Array arr, int index)
+{
+    if (index >= 0 && index <arr.length)
+    {
+        return arr.A[index];
+    }
+    return -1;
+}
+
+////////////////////////////////////////////// Set //////////////////////////////////////////////
+void Set(struct Array *arr, int index, int x)
+{
+    if (index >= 0 && index <arr->length)
+    {
+        arr->A[index] = x;
+    }
+}
+
+////////////////////////////////////////////// Max //////////////////////////////////////////////
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+    for (int i = 1; i <arr.length;i++)
+    {
+        if (arr.A[i]>max)
+        {
+            max = arr.A[i];
+        }
+    }
+    return max;
+}
+
+
+////////////////////////////////////////////// Min //////////////////////////////////////////////
+int Min(struct Array arr)
+{
+    int min = arr.A[0];
+    for (int i = 1; i <arr.length;i++)
+    {
+        if (arr.A[i]<min)
+        {
+            min = arr.A[i];
+        }
+    }
+    return min;
+}
+
+////////////////////////////////////////////// Sum //////////////////////////////////////////////
+int Sum(struct Array arr)
+{
+    int sum = 0;
+    for (int i = 0; i<arr.length;i++)
+    {
+        sum += arr.A[i];
+    }
+    return sum;
+
+}
+
+////////////////////////////////////////////// Avg //////////////////////////////////////////////
+float Avg(struct Array arr)
+{
+    // float avg = Sum(arr)/arr.length;  // <--- this doesnt convert sum to float, must convert before divide
+    return (float)Sum(arr)/arr.length;
+}
+
+
+
+
 int main()
 {
-    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
-
-    printf("%d\n", Delete(&arr, 3));
+    struct Array arr = {{2, 3, 4, 5, 50}, 10, 5};
+    int A[5] = {2,3,4,5,6};
+    printf("%f\n", Avg(arr));
+    // Set(&arr, 4,5);
     Display(arr);
 
     return 0;
