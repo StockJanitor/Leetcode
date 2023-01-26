10720407ea115dc57b10844a2666fa80bae48fdc
unknown <stockjanitor@gmail.com>
2023-01-25 20:55:25 -0800

    mergearray

--- data_structure/array.cpp
+++ data_structure/array.cpp
@@ -209,47 +209,47 @@ void Rearrange(struct Array *arr)
     }
 }
 
-
-
-
-
-
 ////////////////////////////////////////////// Merge Arrays //////////////////////////////////////////////
-struct Array* Merge(struct Array *arr1,struct Array*arr2)
+// returning a pointer
+struct Array *Merge(struct Array *arr1, struct Array *arr2)
 {
-    int i,j,k;
-    i=j=k =0 ;
-    struct Array*arr3= (struct Array*)malloc(sizeof(struct Array));
-    while (i < arr1->length && j<arr2->length)
+    int i, j, k;
+    i = j = k = 0;
+
+    // assign pointer to return later
+    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
+
+    // while i and j are in length
+    while (i < arr1->length && j < arr2->length)
     {
-        if (arr1->A[i]<arr2->A[j])
+        // if arr1 is smaller,
+        if (arr1->A[i] < arr2->A[j])
         {
+            // 1st assign to A[k], then increment k; same applies for i
             arr3->A[k++] = arr1->A[i++];
         }
-        else{
+        else
+        {
             arr3->A[k++] = arr2->A[j++];
         }
     }
 
-    for (;i<arr1->length;i++)
+    for (; i < arr1->length; i++)
     {
         arr3->A[k++] = arr1->A[i];
     }
-    for (;j<arr2->length;j++)
+    for (; j < arr2->length; j++)
     {
         arr3->A[k++] = arr2->A[j];
     }
-    arr3->length = arr1->length+arr2->length;
-    arr3->size=10;
+    arr3->length = arr1->length + arr2->length;
+    arr3->size = 10;
     return arr3;
 }
 
-
-
-
 int main()
 {
-    struct Array arr1 = {{2, 6, 10, 15,25}, 10, 5};
+    struct Array arr1 = {{2, 6, 10, 15, 25}, 10, 5};
     struct Array arr2 = {{3, 4, 7, 18, 20}, 10, 5};
     struct Array *arr3;
     int A[5] = {2, 3, 4, 5, 6};
@@ -258,7 +258,7 @@ int main()
     // Set(&arr, 4,5);
     // Rearrange(&arr);
 
-    arr3=Merge(&arr1,&arr2);
+    arr3 = Merge(&arr1, &arr2);
 
     Display(*arr3);
 
