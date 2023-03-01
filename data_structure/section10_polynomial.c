a9cf50b3ac29ae47e6f0575e1c36c9b72b4935bb
Stock <stockjanitor@gmail.com>
2023-02-28 16:54:12 -0800

    polynomial

--- data_structure/section10_polynomial.c
+++ data_structure/section10_polynomial.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 struct Term
 {
     int coeff;
     int exp;
-
 };
 struct Poly
 {
@@ -15,14 +14,33 @@ struct Poly
 void create(struct Poly *p)
 {
     int i;
-    
-    printf("Numbers of terms");
-    scanf("%d",p->n);
-    p->terms =(struct Term*)malloc(p->n*sizeof(struct Term));
 
+    printf("Numbers of terms");
+    scanf("%d", &p->n);
+    p->terms = (struct Term *)malloc(p->n * sizeof(struct Term));
+
+    printf("Enter terms \n");
+    // enter each coeff and exp
+    for (i = 0; i < p->n; i++)
+    {
+        scanf("%d%d", &p->terms[i].coeff, &p->terms[i].exp);
+    }
+}
+
+void display(struct Poly p)
+{
+    int i;
+    for (i = 0; i < p.n; i++)
+    {
+        printf("%dx%d+", p.terms[i].coeff, p.terms[i].exp);
+    }
+    printf("\n");
 }
 
 int main()
 {
+    struct Poly p1;
+    create(&p1);
+    display(p1);
     return 0;
 }
