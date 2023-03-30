fade02867fcb9759d22771e104fc23f5209496f7
unknown <stockjanitor@gmail.com>
2023-03-29 19:51:00 -0700

    doubledelete

--- data_structure/section11_doubleLL.c
+++ data_structure/section11_doubleLL.c
@@ -18,13 +18,99 @@ void create(int A[],int n)
     first ->prev = first->next = NULL;
     last = first;
 
-    
+    // loop other nodes
+    for (i =1;i<n;i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = last->next;
+        t->prev = last;
+        last->next = t;
+        last = t;
+
+    }
 }
+
+void display(struct Node * p)
+{
+    while(p)
+    {
+        printf("%d ", p->data);
+        p=p->next;
+    }
+    printf("\n");
+}
+
+int length(struct Node*p)
+{
+    int len = 0 ;
+    while (p)
+    {
+        len++;
+        p=p->next;
+
+    }
+    return len;
+}
+
+void insert(struct Node *p, int index, int value)
+{
+    struct Node *t;
+    int i;
+    if (index < 0||index>length(p))
+    {
+        return;
+    }
+    if (index == 0)
+    {
+        t->prev = NULL;
+        t->data = value;
+        t->next = first;
+        first->prev = t;
+
+        first = t;
+    }
+    else
+    {
+        for (i = 0; i<index-1;i++)
+        {
+            p=p->next;
+        }
+        t = (struct Node*)malloc(sizeof(struct Node));
+        t->data = value;
+        t->prev = p;
+        t->next =p->next;
+
+        if (p->next)
+        {
+            p->next->prev = t;
+        }
+        p->next = t;
+    }
+}
+
+int delete(struct Node * p, int index)
+{
+    struct Node *q;
+    int x=-1,i;
+    if (index <1)
+    {
+        
+    }
+}
+
+
 
 
 int main()
 {
-
+    int A[]={10,20,30,40,50};
+    create(A,5);
+    printf("\nLength is : %d\n",length(first));
+    
+    insert(first,2,25);
+    
+    display(first);
 
     return 0;
 }
