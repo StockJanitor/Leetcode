2b87f7cbd7aa950f51b9853cac6e726b325a6f70
stockjanitor <stockjanitor@gmail.com>
2023-07-21 23:42:24 -0700

    stack

--- data_structure/section13_stack.c
+++ data_structure/section13_stack.c
@@ -1,77 +1,36 @@
 /*
 Stack
-
-LIFO - last in first out
-ADT - Abstract data type
+last in first out
 
 */
-
 #include <stdio.h>
 #include <stdlib.h>
-
 struct Stack
 {
-    int size; // current size of storing variable
-    int top;  // top/last of storing variable
+    int size;
+    int top;
     int *S;
 };
 
-void Create(struct Stack *st)
+void create(struct Stack *st)
 {
-    printf("Enter Size: ");
+    printf("Enter Size");
+    // setting the size of stack
     scanf("%d", &st->size);
+
+    // set the top to -1
     st->top = -1;
+
+    // initialize the stack
     st->S = (int *)malloc(st->size * sizeof(int));
 }
 
-void Display(struct Stack s)
+void Display(struct Stack st)
 {
-    // display from the top, reduce 1 by 1 till 0
-    for (int i = s.top; i >= 0; i--)
+    int i;
+    for (i = st.top; i >= 0; i--)
     {
-        printf("%d ", s.S[i]);
+        printf("%d ", st.S[i]);
     }
     printf("\n");
-}
-
-void push(struct Stack *st, int x)
-{
-    if (st->top == st->size - 1)
-    {
-        printf("Stack overflow\n");
-    }
-    else
-    {
-        // increase top, then assign x to new indexx
-        st->top++;
-        st->S[st->top] = x;
-    }
-}
-
-int pop(struct Stack *st)
-{
-    int x = -1;
-    if (st->top == -1)
-    {
-        printf("Stack Underflow\n");
-    }
-    else
-    {
-        x = st->S[st->top--];
-    }
-    return x;
-}
-int main()
-{
-    struct Stack st;
-    Create(&st);
-    push(&st, 10);
-    push(&st, 20);
-    push(&st, 30);
-    push(&st, 40);
-    push(&st, 50);
-    push(&st, 60);
-    Display(st);
-
-    return 0;
 }
